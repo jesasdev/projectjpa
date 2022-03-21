43bd56c7d36acdafb663f96914cf4ac881edf37b
Jesaias Santos <jesasdev@gmail.com>
2022-03-21 00:53:19 -0300

    Projeto criado

---
+++ src/main/java/com/js/projectjpa/ProjectjpaApplication.java
@@ -0,0 +1,13 @@
+package com.js.projectjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/js/projectjpa/ProjectjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.projectjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
