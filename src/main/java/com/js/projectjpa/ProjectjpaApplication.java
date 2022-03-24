7bd69b65ab302d736f25340f85e33da240ebb772
Jesaias Santos <jesasdev@gmail.com>
2022-03-24 16:58:40 -0300

    Produto e assoçiação muitos para muitos

--- src/main/java/com/js/projectjpa/ProjectjpaApplication.java
+++ src/main/java/com/js/projectjpa/ProjectjpaApplication.java
@@ -9,13 +9,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.js.projectjpa.domain.Categoria;
+import com.js.projectjpa.domain.Produto;
 import com.js.projectjpa.repositories.CategoriaRepository;
+import com.js.projectjpa.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjectjpaApplication implements CommandLineRunner {
 	
     @Autowired
 	CategoriaRepository categoriaRepository;
+    
+    @Autowired
+    ProdutoRepository produtoRepository;
     
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectjpaApplication.class, args);
@@ -26,7 +31,16 @@ public class ProjectjpaApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"Computador",5200.00);
+		Produto p2 = new Produto(null,"Impressora",560.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
